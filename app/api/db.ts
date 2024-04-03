93154d637946fc55a8187def4a4997924112a94b
Abisma <imabubakar508@gmail.com>
2024-04-03 14:03:16 +0300

    m

--- app/api/db.ts
+++ app/api/db.ts
@@ -1,50 +1,50 @@
-// import { MongoClient, MongoClientOptions } from 'mongodb';
+// // import { MongoClient, MongoClientOptions } from 'mongodb';
+
+// // let cachedClient: MongoClient;
+
+// // const uri = process.env.MONGODB_URI || '';
+
+// // const options: MongoClientOptions = {
+// //   useNewUrlParser: true,
+// //   useUnifiedTopology: true,
+// // };
+
+// // export async function connectToDatabase() {
+// //   if (cachedClient) {
+// //     return cachedClient;
+// //   }
+
+// //   const client = new MongoClient(uri, options);
+
+// //   await client.connect();
+
+// //   cachedClient = client;
+// //   return client;
+// // }
+// import { MongoClient } from 'mongodb';
+// import { getMedicines } from '@/models/Medicine';
 
 // let cachedClient: MongoClient;
 
-// const uri = process.env.MONGODB_URI || '';
-
-// const options: MongoClientOptions = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
+// const uri: any | string | "undefined" = process.env.MONGODB_URI;
 
 // export async function connectToDatabase() {
 //   if (cachedClient) {
 //     return cachedClient;
 //   }
-
-//   const client = new MongoClient(uri, options);
+// // assign a client variable for the new Mongo client
+//   const client = new MongoClient(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
 
 //   await client.connect();
 
 //   cachedClient = client;
 //   return client;
 // }
-import { MongoClient } from 'mongodb';
-import { getMedicines } from '@/models/Medicine';
 
-let cachedClient: MongoClient;
+// // Export BId as type ( to be used in the useState type annotation where it is imported)
 
-const uri: any | string | "undefined" = process.env.MONGODB_URI;
-
-export async function connectToDatabase() {
-  if (cachedClient) {
-    return cachedClient;
-  }
-// assign a client variable for the new Mongo client
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  await client.connect();
-
-  cachedClient = client;
-  return client;
-}
-
-// Export BId as type ( to be used in the useState type annotation where it is imported)
-
-export type { getMedicines }; 
+// export type { getMedicines }; 
 
